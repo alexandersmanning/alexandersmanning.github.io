4747c7fa94cd59df6d23446052a4b6bc63df2af5
alexandersmanning <alexandersmanning@gmail.com>
2016-12-04 22:35:28 -0800

    added links and airdng to list

--- lib/components/main-page.jsx
+++ lib/components/main-page.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const MainPage = () => (
+const MainPage = () => {
+
+	const outsidePage = (link) => {
+		debugger
+		window.location.assign(link)
+	}
+
+
+	return (
 	<content className="main-page">
 		<section className="title-blurb">
 			<span className="blurb">
@@ -8,7 +16,7 @@ const MainPage = () => (
 			</span>
 		</section>
 		<content className="main-content">
-			<a className="project-content found-sounds" href="www.foundsounds.live">
+			<a className="project-content found-sounds" href="http://www.foundsounds.live" target="_blank">
 				 <img className="project-image" 
 									src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1480899229/cassette_1_a0nwcj.png"
 						/>
@@ -18,7 +26,18 @@ const MainPage = () => (
 					</span>
 				</div>
 			</a>
-			<a className="project-content katamari" href="www.alexandersmanning.com/katamari">
+			<a className="project-content airdng"
+				href="http://www.alexandersmanning.com/AirDnG" target="_blank">
+				 <img className="project-image" 
+									src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1480917169/network_3_hctzry.png"
+						/>
+				<div className="project-overlay">
+					<span className="project-name">
+						AirDnG
+					</span>
+				</div>
+			</a>
+			<a className="project-content katamari" href="http://www.alexandersmanning.com/katamari" target="_blank">
 				 <img className="project-image" 
 									src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1480902753/moon_1_o9pq01.png"
 						/>
@@ -28,8 +47,8 @@ const MainPage = () => (
 					</span>
 				</div>
 			</a>
+			
 		</content>
 	</content>
-)
-
+)}
 export default MainPage;
