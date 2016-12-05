a1e99f8fc8ac5017f647cb92c8a0c4ba464cec92
alexandersmanning <alexandersmanning@gmail.com>
2016-12-05 13:17:58 -0800

    modified layout for media devices

---
+++ lib/components/foundsounds.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FoundSounds = () => {
+	return (
+		<content className="project-page">
+			<section className="title-blurb">
+				<span className="blurb">
+				Found Sounds
+				</span>
+				<span className="sub-blurb">
+					Found Sounds is a responsive single-page concert viewer, displaying live shows across the bay area
+				</span>
+			</section>
+		</content>
+	)
+};
+
+export default FoundSounds
