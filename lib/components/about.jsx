ce9409e1816644fb43aa5bd6edba4d0304a95cd5
alexandersmanning <alexandersmanning@gmail.com>
2016-12-05 22:53:08 -0800

    added angel list link and picture

--- lib/components/about.jsx
+++ lib/components/about.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
 					<span className="small-blurb">
 						Full stack web developer and engineer. Passionate about data, processes, and efficiency. Bagel connoisseur.
 					</span>
-					<img className='about-image' />
+					<img className='about-image' src="http://res.cloudinary.com/ddvdi1pie/image/upload/v1481006493/Manning_fodipl.jpg"/>
 				</section>
 				<content className="main-content-about">
 					<h2 className="name-title">Alex Manning</h2>
@@ -27,12 +27,16 @@ const AboutPage = () => {
 					<content className="contact-link-container">
 						<span className="contact-title">Let's work together</span>
 						<div className="link-group">
-							<a className="contact-link" href="https://github.com/alexandersmanning" target="_blank">
-								<img className="contact-image" src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1481004816/github-logo_avulo9.png"/>
+							<a className="contact-link" href="https://github.com/alexandersmanning" target="_blank" >
+								<img className="contact-image" src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1481004816/github-logo_avulo9.png" alt="Github"/>
 							</a>
-							<a className="contact-link" href="https://www.linkedin.com/in/alexandersmanning/" target="_blank">
-								<img className="contact-image" src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1481004820/linkedin-logo_ruddep.png"/>
+							<a className="contact-link" href="https://angel.co/alex-manning-2" target="_blank" >
+								<img className="contact-image" src="https://res.cloudinary.com/ddvdi1pie/image/upload/c_scale,w_64/v1481006209/69-angellist-128_hai6eu.png" alt="Angel List"/>
 							</a>
+							<a className="contact-link" href="https://www.linkedin.com/in/alexandersmanning/" target="_blank" >
+								<img className="contact-image" src="https://res.cloudinary.com/ddvdi1pie/image/upload/v1481004820/linkedin-logo_ruddep.png" alt="LinkedIn"/>
+							</a>
+							
 						</div>
 					</content>
 			</content>
